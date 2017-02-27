a64c57b9ff9192429eb7624b446d53c6279446d9
jenn <epistemicpolymathic5@gmail.com>
2017-02-26 19:54:37 -0500

    department_Update should be complete, but testing now...

--- department_Update.php
+++ department_Update.php
@@ -5,3 +5,22 @@
  * Date: 2/26/2017
  * Time: 7:27 PM
  */
+
+require_once("database.php");
+
+$department_id = $_POST['department_id'];
+$departmentName = $_POST['departmentName'];
+
+
+$updateQuery = $db->prepare("UPDATE department
+        SET departmentName =  :departmentName
+              
+        WHERE departmentID = :departmentID;");
+
+$updateQuery->execute(array(
+    ":departmentName" => $departmentName,
+    ":departmentID " => $department_id
+));
+$updateQuery->closeCursor();
+include("department_list.php");
+header("location:department_list.php");
